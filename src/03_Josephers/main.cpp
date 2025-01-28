52d0cb78642370f9d3d8fa72bc7c635e72db2b76
arya2004 <arya20j4@gmail.com>
2025-01-28 11:58:42 +0530

    feat: Implement Josephus problem solution in main.cpp

---
+++ src/03_Josephers/main.cpp
@@ -0,0 +1,95 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+
+int Josephus(int n, int k)
+{
+    k--;
+    int arr[n];
+
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = 1;
+    }  
+    int cnt = 0, cut = 0,
+
+        num = 1;
+
+    while (cnt < (n - 1)) {
+
+    
+        while (num <= k) {
+            cut++;
+
+            cut = cut % n;
+            if (arr[cut] == 1) {
+             
+                num++;
+            }
+        }
+
+        num = 1;
+
+        arr[cut] = 0;
+
+        
+        cnt++;
+        cut++;
+
+     
+        cut = cut % n;
+
+
+        while (arr[cut] == 0) {
+            cut++;
+
+
+            cut = cut % n;
+        }
+    }
+
+
+    return cut + 1;
+}
+
+
+int main()
+{
+    int n = 25, k = 2;
+    cout << Josephus(n, k);
+    return 0;
+}
+
